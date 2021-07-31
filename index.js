4b3fad6cd479e118b6737d64f53ca8ad3b7488b8
mage <1275054668@qq.com>
2021-07-31 12:30:15 +0800

    删除两个没用文件

--- index.js
+++
@@ -1,7 +0,0 @@
-//然后，找到 index.js 文件，
-// index.js
-// 本主题样式文件：ananananzhuo.scss
-// 同步预览文章：https://juejin.cn/post/6986405985908686884
-
-// 参数为样式文件 & 文章 ID
-run('ananananzhuo.scss', '6986405985908686884');
