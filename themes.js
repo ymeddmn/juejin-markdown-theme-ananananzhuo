c66e97b11a927578c6913c6340850d4b3912cdf0
mage <1275054668@qq.com>
2021-07-31 08:05:00 +0800

    提交分支
    提交ananananzhuo分支

---
+++ themes.js
@@ -0,0 +1,6 @@
+ananananzhuo: { // 主题名称
+    owner: 'ymeddmn', // github 用户名
+    repo: 'juejin-markdown-theme-ananananzhuo', // 主题仓库名
+    path: 'ananananzhuo.scss', // 主题文件
+    ref: 'a17ed6e', // 提交的 commit hash key
+  },
