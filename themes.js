e51b8608a6932d1a2f574e4b67814a4e914b2b43
mage <1275054668@qq.com>
2021-07-31 08:17:41 +0800

    提交theme

--- themes.js
+++ themes.js
@@ -2,5 +2,5 @@ ananananzhuo: { // 主题名称
     owner: 'ymeddmn', // github 用户名
     repo: 'juejin-markdown-theme-ananananzhuo', // 主题仓库名
     path: 'ananananzhuo.scss', // 主题文件
-    ref: 'a17ed6e', // 提交的 commit hash key
+    ref: 'c66e97b', // 提交的 commit hash key
   },
